fbcb3ab8e1cfdfc2c034400f7b467de927d83959
Joyce Hong <jkim2027@gmail.com>
2023-09-20 10:57:38 -0400

    add JS

---
+++ flask_app/static/script.js
@@ -0,0 +1,8 @@
+function changeColor(element) {
+    element.style.color = "#F47C3C";
+    element.style.textDecoration = "none";
+}
+
+function changeBack(element) {
+    element.style.color = "#325D88"
+}
